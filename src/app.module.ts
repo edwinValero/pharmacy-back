7e4472944aa2e56fe8e86917e01ad3d3a0a1a265
edwin <edwwin527@gmail.com>
2023-09-09 18:51:42 -0400

    feat: implement ORM (#2)
    Co-authored-by: edwin valero <edwin.valero@cox2m.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from './data-access/connection-option';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(connectionOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
